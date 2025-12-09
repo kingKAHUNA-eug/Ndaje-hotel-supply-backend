2aee651cc9aa56f2385ff6c346192f24d824114b
Kahuna <ntwariprince01@gmail.com>
2025-12-09 14:49:52 +0200

    qoute controller

--- src/controllers/quoteController.js
+++ src/controllers/quoteController.js
@@ -209,7 +209,7 @@ const getManagerQuotes = async (req, res) => {
     res.json({
       success: true,
       message: 'Manager quotes retrieved',
-      data: { quotes }
+      data:  quotes 
     });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
